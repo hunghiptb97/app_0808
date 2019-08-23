827cbbf89ca1e14814af36c8b2c0ee4b6df117c9
hung <hunghipbt97@gmail.com>
2019-08-23 11:39:18 +0700

    Add user following

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com' # Don't use this literally; use your local dev host instead
-  # Use this on the cloud IDE.
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  host = 'localhost:3000'                     # Local server
+config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Use this if developing on localhost.
   # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,5 +6,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_new_url
     assert_response :success
   end
+    test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 
 end
